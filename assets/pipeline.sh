7dae450dacb36051f3fa1f80cc572778052974f5
cc339 <chrostopher.cason@gmail.com>
2022-05-20 15:28:29 +1200

    Adding automatic Git committing to pipeline.sh

--- assets/pipeline.sh
+++ assets/pipeline.sh
@@ -7,8 +7,14 @@ npm run build
 echo "(2) Committing Files to Version Control..."
 git init
 git add .
+if sam build; then
 git commit -m "Committing Files"
 git push origin master
+echo "(2) Commit Successful!"
 
+else
+  echo "(2) Commit Failure. Build error"
+  exit
+fi
 echo "(3) Launching Application..."
 npm run start
