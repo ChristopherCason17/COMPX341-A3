b8b1b86459e2eaab522ef0c316584bee5fbf6c44
cc339 <chrostopher.cason@gmail.com>
2022-05-20 15:49:15 +1200

    Committing Files

---
+++ pipeline.sh
@@ -0,0 +1,16 @@
+echo "(0) Preparing Pipeline..."
+echo "(1) Installing npm..."
+
+npm install
+npm run build
+
+echo "(2) Committing Files to Version Control..."
+git init
+git add .
+git commit -m "Committing Files"
+git push origin master
+
+echo "(2) Commit Successful!"
+
+echo "(3) Launching Application..."
+npm run start
---
+++ src/modules/category/categoryNavBar/categories.ts
@@ -0,0 +1,10 @@
+//
+export const categories = {
+  cooks: "Cookbooks",
+  database: "Database",
+  fairy: "Fairy Tales",
+  scifi: "Science Fiction",
+  home: "Home Improvement",
+  cars: "Cars",
+  woodwork: "Woodwork",
+}
